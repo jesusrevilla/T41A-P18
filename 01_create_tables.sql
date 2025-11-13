1df5d3f61d55c6273082319983da354915d08644
Karina123Kma <upslpbd@gmail.com>
2025-11-13 16:23:10 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -4,12 +4,20 @@ CREATE TABLE productos (
     etiquetas TEXT[]
 );
 
+CREATE TABLE IF NOT EXISTS usuarios (
+    id SERIAL PRIMARY KEY,
+    data JSONB NOT NULL
+);
+
 
 INSERT INTO productos (nombre, etiquetas) VALUES
 ('Laptop', ARRAY['tecnología', 'portátil', 'oficina']),
 ('Silla ergonómica', ARRAY['muebles', 'oficina']),
 ('Smartphone', ARRAY['tecnología', 'móvil', 'comunicación']);
 
+INSERT INTO usuarios (data) VALUES
+    ('{"nombre": "Ana", "activo": true, "edad": 30}'),
+    ('{"nombre": "Juan", "activo": false, "edad": 25}');
 
 SELECT id, nombre, etiquetas
 FROM productos
