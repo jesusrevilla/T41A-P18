5633de0217eb845f24d7ef4eb4407e5602326eec
AngelSaucedo-dev <ange1eduard08122003@gmail.com>
2025-11-13 13:29:05 -0600

    Add SQL script to insert data into productos and empleados

---
+++ Create 02_insert_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO productos (nombre, etiquetas) VALUES
+('Laptop', ARRAY['tecnología', 'portátil']),
+('Smartphone', ARRAY['tecnología', 'comunicación']),
+('Taza', ARRAY['hogar', 'cerámica']);
+
+INSERT INTO empleados (nombre, jefe_id) VALUES
+('Ana', NULL),
+('Luis', 1),
+('Marta', 2),
+('Pedro', 2),
+('Sofía', 3);
