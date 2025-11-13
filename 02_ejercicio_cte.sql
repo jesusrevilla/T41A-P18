65487111fced01e7ac43713b3fbb74a7b389f2e5
Rafael López <126181633+Refex38@users.noreply.github.com>
2025-11-12 19:33:31 -0600

    Create 02_ejercicio_cte.sql

---
+++ 02_ejercicio_cte.sql
@@ -0,0 +1,29 @@
+CREATE TABLE empleados (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT,
+    jefe_id INT
+);
+
+INSERT INTO empleados (nombre, jefe_id) VALUES
+('Maria', NULL),
+('Jose', 1),
+('Juan', 2),
+('Brandon', 2),
+('Rafael', 2);
+
+WITH RECURSIVE subordinados_de AS (
+    SELECT id, nombre, jefe_id FROM empleados WHERE jefe_id = 2
+    UNION ALL
+    SELECT e.id, e.nombre, e.jefe_id
+    FROM empleados e
+    INNER JOIN subordinados_de sd ON e.jefe_id = sd.id
+)
+
+SELECT
+    j.id AS id_jefe,
+    j.nombre AS nombre_jefe,
+    sd.id AS id_empleado,
+    sd.nombre AS nombre_empleado
+FROM
+    subordinados_de sd
+LEFT JOIN empleados j ON sd.jefe_id = j.id;
