ea176b271ee61d178ef95ec13eec7b74003dbed1
Rafael López <126181633+Refex38@users.noreply.github.com>
2025-11-12 21:53:17 -0600

    Update 02_ejercicio_cte.sql

--- 02_ejercicio_cte.sql
+++ 02_ejercicio_cte.sql
@@ -11,19 +11,33 @@ INSERT INTO empleados (nombre, jefe_id) VALUES
 ('Brandon', 2),
 ('Rafael', 2);
 
-WITH RECURSIVE subordinados_de AS (
-    SELECT id, nombre, jefe_id FROM empleados WHERE jefe_id = 2
-    UNION ALL
-    SELECT e.id, e.nombre, e.jefe_id
-    FROM empleados e
-    INNER JOIN subordinados_de sd ON e.jefe_id = sd.id
+CREATE OR REPLACE FUNCTION obtener_subordinados_de(
+    p_id_jefe INT
 )
-
-SELECT
-    j.id AS id_jefe,
-    j.nombre AS nombre_jefe,
-    sd.id AS id_empleado,
-    sd.nombre AS nombre_empleado
-FROM
-    subordinados_de sd
-LEFT JOIN empleados j ON sd.jefe_id = j.id;
+RETURNS TABLE(
+    id_jefe INT,
+    nombre_jefe TEXT,
+    id_empleado INT,
+    nombre_empleado TEXT
+) AS $$
+BEGIN
+    RETURN QUERY
+    WITH RECURSIVE subordinados_de AS (
+        SELECT id, nombre, jefe_id 
+        FROM empleados 
+        WHERE jefe_id = p_id_jefe
+        UNION ALL
+        SELECT e.id, e.nombre, e.jefe_id
+        FROM empleados e
+        INNER JOIN subordinados_de sd ON e.jefe_id = sd.id
+    )
+    SELECT
+        j.id AS id_jefe,
+        j.nombre AS nombre_jefe,
+        sd.id AS id_empleado,
+        sd.nombre AS nombre_empleado
+    FROM
+        subordinados_de sd
+    LEFT JOIN empleados j ON sd.jefe_id = j.id;
+END;
+$$ LANGUAGE plpgsql;
