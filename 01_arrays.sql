507ecc4b8095487e6efe0db9de1910d6d4f225da
menaxixi <179800@upslp.edu.mx>
2025-11-18 19:18:38 -0600

    Update 01_arrays.sql

--- 01_arrays.sql
+++ 01_arrays.sql
@@ -1,3 +1,4 @@
+-- Crear tabla con ARRAY de etiquetas
 DROP TABLE IF EXISTS productos CASCADE;
 CREATE TABLE productos (
     id SERIAL PRIMARY KEY,
@@ -5,11 +6,13 @@ CREATE TABLE productos (
     etiquetas TEXT[]
 );
 
+-- Insertar productos con diferentes etiquetas
 INSERT INTO productos (nombre, etiquetas) VALUES
 ('Laptop', ARRAY['tecnología', 'portátil', 'oficina']),
 ('Teléfono', ARRAY['tecnología', 'móvil', 'comunicación']),
 ('Refrigerador', ARRAY['hogar', 'electrodoméstico']);
 
+-- Consultar productos con etiqueta 'tecnología'
 SELECT nombre
 FROM productos
 WHERE 'tecnología' = ANY(etiquetas);
