3f6923ef8f46ab26ee3c86688d018b645c1de3f4
ItsMusa17 <160455957+ItsMusa17@users.noreply.github.com>
2025-11-18 19:44:16 -0600

    Update 01_Create_Table.sql

--- 01_Create_Table.sql
+++ 01_Create_Table.sql
@@ -1,11 +1,18 @@
-DROP TABLE IF EXISTS empleados;
+--- Ejercicio 1: Arrays (Productos) ---
+CREATE TABLE productos (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    etiquetas TEXT[]
+);
 
+--- Ejercicio 2: CTE Recursiva (Empleados) ---
 CREATE TABLE empleados (
     id SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    jefe_id INTEGER,
+    nombre TEXT NOT NULL,
+    jefe_id INT,
+    
     CONSTRAINT fk_jefe
-        FOREIGN KEY (jefe_id)
+        FOREIGN KEY(jefe_id) 
         REFERENCES empleados(id)
         ON DELETE SET NULL
 );
